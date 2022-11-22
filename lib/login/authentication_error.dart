31e63489e0ecd3bd66a20d447ea0bd8359165be4
esumaine <e.sumaine.0@gmail.com>
2022-11-22 17:29:04 +0900

    aa

--- lib/login/authentication_error.dart
+++ lib/login/authentication_error.dart
@@ -6,9 +6,6 @@ class Authentication_error {
   // ログイン時の日本語エラーメッセージ
   login_error_msg(String errorCode) {
     String errorMsg;
-
-
-
     if (errorCode == 'invalid-email') {
       errorMsg = '有効なメールアドレスを入力してください。';
     } else if (errorCode == 'user-not-found') {
